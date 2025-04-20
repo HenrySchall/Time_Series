55b7b74cf9850ce1135ef36c02f263f84b280fa6
HenrySchall <henrique.schall@outlook.com>
2025-04-20 00:07:35 -0300

    .

---
+++ Python/AI/TimeGPT.py
@@ -0,0 +1,18 @@
+import string as string
+import random as random
+import radian as rd
+import pandas as pd
+import numpy as np
+import scipy.stats as stats
+import datetime  
+import seaborn as sns
+import seaborn.objects as so
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.pylab import rcParams
+import statsmodels.tsa.stattools as sm
+import plotly.express as px
+import math as math
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.seasonal import seasonal_decompose as decompose
+import statsmodels.api as sm
