e8626c5d50b5f8e4d6d8a082313db0c7f8336bd0
HenrySchall <henrique.schall@outlook.com>
2025-09-04 12:45:51 -0300

    .

--- Python/Univariate/1.Introduction.py
+++ Python/Univariate/1.Introduction.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
+import scipy.stats as stats
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from matplotlib.pylab import rcParams
 from statsmodels.tsa.seasonal import seasonal_decompose as decompose
-from scipy import stats
 
 # Configuração do padrão de medidas do plot dos gráficos para ficar visivelmente mais agradáveis
 rcParams['figure.figsize'] = 15, 6
@@ -68,8 +68,7 @@ index_anual
 serie_index = pd.Series(df_anual['Valores'].values, index = index_anual)
 serie_index.plot()
 
-# Customizando o gráfico
-
+### Customizando o gráfico ###
 ### Título
 ax.set_title("Série Temporal Anual", color="white", fontsize=14)
 
@@ -89,13 +88,99 @@ for spine in ax.spines.values():
 
 plt.show()
 
+######################
+### Autocorrelação ###
+######################
 
+np.random.seed(6)
+dados1 = np.random.normal(0,1,72)
+dados1
 
+serie = pd.Series(dados1)
+serie.plot()
+plt.show()
 
+## FAC ###
+plot_acf(serie, lags=15)
+plt.show()
 
+## FACP ###
+plot_pacf(serie, lags=30)
+plt.show()
 
+#########################
+### Passeio Aleatório ###
+#########################
 
+from random import sample, random
 
+# Série Passeio aleatório 1
+# Criando dados
+dados_alet = sample(range(100), k=41) # k -> valores retirados de 100
+dados_alet
+
+# Criando período
+periodo = list(range(1980, 2021))
+periodo
+
+serie_alet = pd.Series(dados_alet, index = periodo)
+serie_alet
+serie_alet.plot()
+plt.show()
+
+## Série Passeio aleatório 2
+# Criando dados
+serie2 = list()
+serie2.append(-1 if random() < 0.5 else 1)
+for i in range(1, 1000):
+    movimento = -1 if random() < 0.5 else 1
+    valor = serie2[i-1] + movimento
+    serie2.append(valor)
+
+serie2 = pd.Series(serie2)
+serie2.plot()
+plt.show()
+
+##############
+### Testes ###
+##############
+
+### Testes de Normalidade ###
+# - H0: resíduos normalmente distribuídos (p > 0.05)
+# - H1: resíduos não são normalmente distribuídos (p <= 0.05)
+stats.shapiro(serie1)
+
+### Testes de Estacionaridade ###
+# Teste pp (Philips-Perron)
+# - H0 = é estacionária: p > 0.05
+# - H1 = não é estacionária: p <= 0.05
+pp_test = sm.phillips_perron(serie1)
+
+# Renomendando saídas 
+pp_test_output = {'Estatítica do teste': pp_test[0], 'p-value': pp_test[1], 'Número de lags': pp_test[2], 'Número de Observações': pp_test[3], 'Valores Críticos': pp_test[4]}
+pp_test_output
+
+### Teste Kwiatkowski-Phillips-Schmidt-Shin ###
+# - H0 = é estacionária: teste estatístico < valor crítico
+# - H1 = não é estacionária: teste estatístico >= valor crítico
+kpss_test = sm.kpss(serie1)
+
+kpss_test_output = {'Estatítica do teste': kpss_test[0], 'p-value': kpss_test[1], 'Número de lags': kpss_test[2], 'Valores Críticos': kpss_test[3]}
+kpss_test_output
+
+### Teste Dickey Fuller ###
+# - H0 = é estacionária:  teste estatístico < valor crítico
+# - H1 = não é estacionária: teste estatístico > valor crítico
+df_test = sm.adfuller(serie1)
+
+df_test_output = {'Estatítica do teste': df_test[0], 'p-value': df_test[1], 'Número de lags': df_test[2], 'Número de Observações': df_test[3], 'Valores Críticos': df_test[4]}
+df_test_output
+
+### Testes de Autocorrelação (Ljung-Box ) ###
+# - H0: não autocorrelacionados (p > 0.05)
+# - H1: são autocorrelacionados (p <= 0.05)
+ljung_box_test = sm.stats.acorr_ljungbox(serie1, lags=[10], return_df=True)
+ljung_box_test
 
 
 
@@ -156,26 +241,6 @@ serie1 = pd.Series(dados['valores'].values, index = periodo)
 serie1.plot()
 plt.show()
 
-######################
-### Autocorrelação ###
-######################
-
-np.random.seed(6)
-dados1 = np.random.normal(0,1,72)
-dados1
-
-serie = pd.Series(dados1)
-serie.plot()
-plt.show()
-
-## FAC ###
-plot_acf(serie, lags=15)
-plt.show()
-
-## FACP ###
-plot_pacf(serie, lags=30)
-plt.show()
-
 ### Case Manchas Solares ###
 
 manchas_solares = sm.datasets.sunspots.load_pandas().data
@@ -191,87 +256,6 @@ plt.show()
 
 plot_pacf(serie_m, lags=30)
 plt.show()
-
-#########################
-### Passeio Aleatório ###
-#########################
-
-from random import sample, random
-
-# Série Passeio aleatório 1
-# Criando dados
-dados_alet = sample(range(100), k=41) # k -> valores retirados de 100
-dados_alet
-
-# Criando período
-periodo = list(range(1980, 2021))
-periodo
-
-serie_alet = pd.Series(dados_alet, index = periodo)
-serie_alet
-serie_alet.plot()
-plt.show()
-
-## Série Passeio aleatório 2
-# Criando dados
-serie2 = list()
-serie2.append(-1 if random() < 0.5 else 1)
-for i in range(1, 1000):
-    movimento = -1 if random() < 0.5 else 1
-    valor = serie2[i-1] + movimento
-    serie2.append(valor)
-
-serie2 = pd.Series(serie2)
-serie2.plot()
-plt.show()
-
-##############
-### Testes ###
-##############
-
-### Testes de Normalidade ###
-
-# - H0: resíduos normalmente distribuídos (p > 0.05)
-# - H1: resíduos não são normalmente distribuídos (p <= 0.05)
-stats.shapiro(serie1)
-
-# Verificando se segue uma distribuição normal (graficamente)
-stats.probplot(serie1, dist="norm", plot=plt)
-plt.title("Normal QQ plot")
-plt.show()
-
-### Testes de Estacionaridade ###
-
-# Teste pp (Philips-Perron)
-# - H0 = é estacionária: p > 0.05
-# - H1 = não é estacionária: p <= 0.05
-pp_test = sm.phillips_perron(serie1)
-
-# Renomendando saídas 
-pp_test_output = {'Estatítica do teste': pp_test[0], 'p-value': pp_test[1], 'Número de lags': pp_test[2], 'Número de Observações': pp_test[3], 'Valores Críticos': pp_test[4]}
-pp_test_output
-
-# Teste KPSS (Kwiatkowski-Phillips-Schmidt-Shin)
-# - H0 = é estacionária: teste estatístico < valor crítico
-# - H1 = não é estacionária: teste estatístico >= valor crítico
-kpss_test = sm.kpss(serie1)
-
-kpss_test_output = {'Estatítica do teste': pp_test[0], 'p-value': pp_test[1], 'Número de lags': pp_test[2], 'Valores Críticos': pp_test[3]}
-kpss_test_output
-
-# Teste Dickey Fuller
-# - H0 = é estacionária:  teste estatístico < valor crítico
-# - H1 = não é estacionária: teste estatístico > valor crítico
-df_test = sm.adfuller(serie1)
-
-df_test_output = {'Estatítica do teste': pp_test[0], 'p-value': pp_test[1], 'Número de lags': pp_test[2], 'Número de Observações': pp_test[3], 'Valores Críticos': pp_test[4]}
-df_test_output
-
-### Testes de Autocorrelação ###
-
-# Teste de Ljung-Box 
-# - H0: não autocorrelacionados (p > 0.05)
-# - H1: são autocorrelacionados (p <= 0.05)
 
 ####################
 ### Decomposição ###
